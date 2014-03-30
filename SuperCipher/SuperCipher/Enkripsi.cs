57198a1bd7961f04d1a31f4cd4bb58b4691779d2
Sonny Theo Tumbur Manurung <sonthehottest@gmail.com>
2014-03-30 01:22:10 -0700

    Added Fibonacci Class + Tranpose for Encryption

--- SuperCipher/SuperCipher/Enkripsi.cs
+++ SuperCipher/SuperCipher/Enkripsi.cs
@@ -8,6 +8,40 @@ namespace SuperCipher
 {
     class Enkripsi
     {
+        public byte[] transpose(byte[] b)
+        {
+            UInt32 length = (UInt32)b.Length;
 
+            Fibonacci fib = new Fibonacci();
+            fib.start();
+
+            //initialize value
+            UInt32 i = fib.next(), j = fib.next();
+
+            //from-start tranposition
+            while(j<length)
+            {
+                Byte temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+
+                i = j; j = fib.next();
+            }
+
+            //from-end transposition
+            fib.start();
+            i = fib.next(); j = fib.next();
+
+            while(j<length)
+            {
+                Byte temp = b[length-i];
+                b[length-i] = b[length-j];
+                b[length-j] = temp;
+
+                i = j; j = fib.next();
+            }
+
+            return b;
+        }
     }
 }
---
+++ SuperCipher/SuperCipher/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCipher
+{
+    class Fibonacci
+    {
+        //max size: 4GB
+        private UInt32 a;
+        private UInt32 b;
+
+        public void start()
+        {
+            a = 0;
+            b = 1;
+        }
+
+        //value starts from 1
+        public UInt32 next()
+        {
+            UInt32 temp = a;
+            a = b;
+            b = temp + b;
+            return a;
+        }
+    }
+}
