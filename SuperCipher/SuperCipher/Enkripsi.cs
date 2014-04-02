d26b6b43dd8945b03f443713249742ec3638cf21
Sonny Theo Tumbur Manurung <sonthehottest@gmail.com>
2014-04-03 05:51:04 +0700

    Fixed CFB Encryption

--- SuperCipher/SuperCipher/Enkripsi.cs
+++ SuperCipher/SuperCipher/Enkripsi.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SuperCipher
 {
-    class Enkripsi
+    public class Enkripsi
     {
         byte[][] internalKey; //digunakan pada generateInternalKey, addRoundKey
 
