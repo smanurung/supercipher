0b4d66352564f8d460040cbda555d7accfa74720
Sonny Theo Tumbur Manurung <sonthehottest@gmail.com>
2014-04-02 19:42:20 -0700

    Added CFB Decryption

--- SuperCipher/SuperCipher/CFB.cs
+++ SuperCipher/SuperCipher/CFB.cs
@@ -13,12 +13,12 @@ namespace SuperCipher
         private String key;
         private String iv;
 
-        public CFB(String plain, String key, String iv)
+        public CFB(String plain, String cipher, String key, String iv)
         {
             this.plain = plain;
             this.key = key;
             this.iv = iv;
-            this.cipher = "";
+            this.cipher = cipher;
         }
 
         public String encrypt()
@@ -43,21 +43,23 @@ namespace SuperCipher
                 byte[] pi = new byte[blok];
                 byte[] ci = new byte[blok];
 
-                if((pbyte.Length - (i * blok)) >= blok)
-                {
-                    pi = pbyte.Skip(i * blok).Take(blok).ToArray();
-                }
-                else
-                {
-                    pi = pbyte.Skip(i * blok).Take(pbyte.Length - (i * blok)).ToArray();
-                    int padsize = blok - pbyte.Length + (i * blok);
+                //if((pbyte.Length - (i * blok)) >= blok)
+                //{
+                //    pi = pbyte.Skip(i * blok).Take(blok).ToArray();
+                //}
+                //else
+                //{
+                //    pi = pbyte.Skip(i * blok).Take(pbyte.Length - (i * blok)).ToArray();
+                //    int padsize = blok - pbyte.Length + (i * blok);
 
-                    //insert padding 0
-                    for(int k = 0; k < padsize; k++)
-                    {
-                        pi[blok - k] = 0;
-                    }
-                }
+                //    insert padding 0
+                //    for(int k = 0; k < padsize; k++)
+                //    {
+                //        pi[blok - k] = 0;
+                //    }
+                //}
+
+                pi = pbyte.Skip(i * blok).Take(pbyte.Length - (i * blok)).ToArray();
 
                 ci = xor(pi, ikey);
                 for (int m = 0; m < blok; m++)
@@ -69,7 +71,8 @@ namespace SuperCipher
                 register = cbyte;
             }
 
-            return Encoding.ASCII.GetString(cbyte);
+            this.cipher = Encoding.ASCII.GetString(cbyte);
+            return this.cipher;
         }
 
         public byte[] xor(byte[] i, byte[] key)
@@ -86,7 +89,40 @@ namespace SuperCipher
 
         public String decrypt()
         {
-            return null;
+            //form shift register
+            byte[] register = Encoding.ASCII.GetBytes(this.iv);
+            byte[] ikey = new byte[register.Length];
+
+            //start decryption
+            Enkripsi enkripsi = new Enkripsi();
+
+            Int32 blok = Encoding.ASCII.GetBytes(this.key).Length;
+            byte[] cbyte = Encoding.ASCII.GetBytes(this.cipher);
+            byte[] pbyte = new byte[cbyte.Length];
+
+            for (int i = 0; i < (cbyte.Length / blok); i++)
+            {
+                //get internal key
+                ikey = enkripsi.encrypt(Encoding.ASCII.GetBytes(key), register);
+
+                //get block-sized plaintext
+                byte[] ci = new byte[blok];
+                byte[] pi = new byte[blok];
+
+                ci = cbyte.Skip(i * blok).Take(cbyte.Length - (i * blok)).ToArray();
+
+                pi = xor(ci, ikey);
+                for (int m = 0; m < blok; m++)
+                {
+                    pbyte[i * blok + m] = pi[m];
+                }
+
+                //wrap register
+                register = cbyte;
+            }
+
+            this.plain = Encoding.ASCII.GetString(pbyte);
+            return this.plain;
         }
     }
 }
